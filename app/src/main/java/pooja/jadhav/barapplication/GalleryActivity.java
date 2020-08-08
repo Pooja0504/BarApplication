4f70a2b480ac69ace1f3a8030b21272713bba669
Kenisha <jadhavpoojasopan@gmail.com>
2020-08-08 11:11:49 +0530

    Initial commit

---
+++ app/src/main/java/pooja/jadhav/barapplication/GalleryActivity.java
@@ -0,0 +1,14 @@
+package pooja.jadhav.barapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GalleryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery);
+    }
+}
---
+++ app/src/main/java/pooja/jadhav/barapplication/MainActivity.java
@@ -0,0 +1,62 @@
+package pooja.jadhav.barapplication;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menubar, menu);
+        setContentView(R.layout.activity_main);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.camera:
+                camera();
+                return true;
+            case R.id.settings:
+                setting();
+                return true;
+            case R.id.gallery:
+                gallery();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void gallery() {
+        Intent intent=new Intent(MainActivity.this,GalleryActivity.class);
+        startActivity(intent);
+    }
+
+    private void setting() {
+        Intent intent=new Intent(MainActivity.this,SettingsActivity.class);
+        startActivity(intent);
+    }
+
+    private void camera() {
+        Intent intent=new Intent(MainActivity.this,CameraActivity.class);
+        startActivity(intent);
+    }
+}
